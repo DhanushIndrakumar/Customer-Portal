1a93be499d1fce7a8f43c8c5793eeb75ca3a4cdd
Admin <dhanushindrakumar123@gmail.com>
2024-01-30 18:24:03 +0530

    done with creating files and User entity

---
+++ company/src/main/java/customer/company/entities/User.java
@@ -0,0 +1,71 @@
+package customer.company.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Table(name="Users")
+@Data
+
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String first_name;
+
+    private String last_name;
+
+    private String street;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private String email;
+
+    private String phone;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+}
